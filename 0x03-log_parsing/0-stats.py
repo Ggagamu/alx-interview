91f74a08c752b8b58b2141e8ce957a8087a92e6c
Isaac <mugagga10@gmail.com>
2024-06-20 21:14:34 +0000

    interview

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,48 +1,37 @@
 #!/usr/bin/python3
-"""
-Module documentation for a script that reads stdin line by line
-and computes metrics:
-"""
+'''a script that reads stdin line by line and computes metrics'''
+
+
 import sys
 
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
+total_size = 0
+counter = 0
 
-def print_statistics(total_size, status_codes):
-    """ Prints status codes """
-    print(f"File size: {total_size}")
-    for code in sorted(status_codes):
-        print(f"{code}: {status_codes[code]}")
+try:
+    for line in sys.stdin:
+        line_list = line.split(" ")
+        if len(line_list) > 4:
+            code = line_list[-2]
+            size = int(line_list[-1])
+            if code in cache.keys():
+                cache[code] += 1
+            total_size += size
+            counter += 1
 
+        if counter == 10:
+            counter = 0
+            print('File size: {}'.format(total_size))
+            for key, value in sorted(cache.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
 
-def parse_line(line):
-    """ Returns parse lines """
-    parts = line.split()
-    if len(parts) >= 7 and parts[5].isdigit():
-        return int(parts[5]), int(parts[6])
-    return None
+except Exception as err:
+    pass
 
-
-def main():
-    """ Entry point """
-    total_size = 0
-    status_codes = {}
-
-    try:
-        for i, line in enumerate(sys.stdin, start=1):
-            data = parse_line(line)
-            if data:
-                file_size, status_code = data
-                total_size += file_size
-                status_codes[status_code] = \
-                    status_codes.get(status_code, 0) + 1
-
-            if i % 10 == 0:
-                print_statistics(total_size, status_codes)
-
-    except KeyboardInterrupt:
-        pass  # Handles keyboard interruption
-
-    print_statistics(total_size, status_codes)
-
-
-if __name__ == "__main__":
-    main()
+finally:
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(cache.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
